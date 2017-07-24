648da178c92a4069ee53e39e5fb62258ee8353af
Nonchalant <afrontier829@gmail.com>
2017-07-25 01:28:06 +0900

    mac

--- Sources/AppIcon/main.swift
+++ Sources/AppIcon/main.swift
@@ -12,21 +12,23 @@ import AppIconCore
 let main = command(Argument<String>("base image (1024x1024.png)", description: "The name of base image"),
                    Option("icon-name", "AppIcon", description: "The name of generated icon name"),
                    Option("output-path", "AppIcon", description: "The name of generated appiconset name"),
-                   Flag("ipad", description: "Whether or not to generate ipad icon")) { base, iconName, path, ipad in
+                   Flag("ipad", description: "Whether or not to generate ipad icon"),
+                   Flag("mac", description: "Whether or not to generate mac icon")) { base, iconName, path, ipad, mac in
     guard base.hasSuffix(".png") else {
         throw ArgumentError.missingValue(argument: "base image (1024x1024.png)")
     }
 
     let outputPath = "\(path).appiconset"
+    let platforms = Platform.platforms(ipad: ipad, mac: mac)
 
     do {
-        try ImageExtractor.extract(input: (base, ipad), output: (iconName, outputPath))
+        try ImageExtractor.extract(input: (base, platforms), output: (iconName, outputPath))
     } catch {
         print("Image Extraction Error is occured 😱")
     }
 
     do {
-        try JSONExtractor.extract(input: ipad, output: (iconName, outputPath))
+        try JSONExtractor.extract(input: platforms, output: (iconName, outputPath))
     } catch {
         print("JSON Extraction Error is occured 😱")
     }
--- Tests/AppIconTests/AppIconCore/AppIconsTests.swift
+++ Tests/AppIconTests/AppIconCore/AppIconsTests.swift
@@ -12,49 +12,38 @@ import XCTest
 
 class AppIconsTests: XCTestCase {
     func testDefaultAll() {
-        let target = AppIcons.all()
+        let target = AppIcons.all(with: [.iphone])
         XCTAssertEqual(target, [
-            AppIconSet(baseSize: 20.0),
-            AppIconSet(baseSize: 29.0),
-            AppIconSet(baseSize: 40.0),
-            AppIconSet(baseSize: 60.0)
+            AppIconSet(baseSize: 20.0, platform: .iphone),
+            AppIconSet(baseSize: 29.0, platform: .iphone),
+            AppIconSet(baseSize: 40.0, platform: .iphone),
+            AppIconSet(baseSize: 60.0, platform: .iphone)
         ])
     }
 
     func testAllWithIPad() {
-        let target = AppIcons.all(ipad: true)
+        let target = AppIcons.all(with: [.iphone, .ipad])
         XCTAssertEqual(target, [
-            AppIconSet(baseSize: 20.0),
-            AppIconSet(baseSize: 29.0),
-            AppIconSet(baseSize: 40.0),
-            AppIconSet(baseSize: 60.0),
-            AppIconSet(baseSize: 20.0, ipad: true),
-            AppIconSet(baseSize: 29.0, ipad: true),
-            AppIconSet(baseSize: 40.0, ipad: true),
-            AppIconSet(baseSize: 76.0, ipad: true),
-            AppIconSet(baseSize: 83.5, ipad: true, scales: [.twice])
+            AppIconSet(baseSize: 20.0, platform: .iphone),
+            AppIconSet(baseSize: 29.0, platform: .iphone),
+            AppIconSet(baseSize: 40.0, platform: .iphone),
+            AppIconSet(baseSize: 60.0, platform: .iphone),
+            AppIconSet(baseSize: 20.0, platform: .ipad),
+            AppIconSet(baseSize: 29.0, platform: .ipad),
+            AppIconSet(baseSize: 40.0, platform: .ipad),
+            AppIconSet(baseSize: 76.0, platform: .ipad),
+            AppIconSet(baseSize: 83.5, platform: .ipad, scales: [.twice])
         ])
     }
 
-    func testAppIconSet() {
-        let target = AppIcons.all().first
-
-        // AppIconSet
-        XCTAssertNotNil(target)
-
-        // Scales
-        XCTAssertEqual(target!.all.count, 2)
-
-        // Twice
-        XCTAssertEqual(target!.all[0].baseSize, 20.0)
-        XCTAssertEqual(target!.all[0].baseSizeStr, "20")
-        XCTAssertEqual(target!.all[0].size, 40.0)
-        XCTAssertEqual(target!.all[0].name(iconName: "iconName2x"), "iconName2x-20.0x20.0@2x.png")
-
-        // Triple
-        XCTAssertEqual(target!.all[1].baseSize, 20.0)
-        XCTAssertEqual(target!.all[1].baseSizeStr, "20")
-        XCTAssertEqual(target!.all[1].size, 60.0)
-        XCTAssertEqual(target!.all[1].name(iconName: "iconName3x"), "iconName3x-20.0x20.0@3x.png")
+    func testAllWithMac() {
+        let target = AppIcons.all(with: [.mac])
+        XCTAssertEqual(target, [
+            AppIconSet(baseSize: 16.0, platform: .mac),
+            AppIconSet(baseSize: 32.0, platform: .mac),
+            AppIconSet(baseSize: 128.0, platform: .mac),
+            AppIconSet(baseSize: 256.0, platform: .mac),
+            AppIconSet(baseSize: 512.0, platform: .mac)
+        ])
     }
 }
