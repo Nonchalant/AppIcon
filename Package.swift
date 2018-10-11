f8bfc710ba4eb9a1a785c0f252af7c51d0a7437f
Alexey Korolev <alphatroya@gmail.com>
2018-10-11 09:50:12 +0300

    Add Mint support

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "AppIcon",
+    products: [
+        .executable(name: "AppIcon", targets: ["AppIcon"])
+    ],
     dependencies: [
         .package(url: "https://github.com/kylef/Commander", from: "0.8.0"),
         .package(url: "https://github.com/kareman/SwiftShell.git", "4.0.0"..<"5.0.0"),
